795ff3a061a30d08f1511996a3c7477eeedec7df
Ema Donev <emadonev@icloud.com>
2023-08-05 14:07:22 +0200

    big changes

---
+++ src/ZTF_data.py
@@ -0,0 +1,60 @@
+'''
+This Python file contains 2 functions which are used for querying ZTF light curves using equatorial
+coordinates of LINEAR light curves.
+'''
+
+def getZTFlightcurve(ra:float, 
+                     dec:float, 
+                     iD,
+                     radius:float=3.0):
+    """Defines a function for acessing the light curve data based on which area of the sky it should search
+
+    Arguments:
+        ra (float): rectascension coordinate of star
+        dec (float): declination coordinate of star
+        radius(float, default=3.0): how wide should the search radius be
+    
+    """
+    try: 
+        lcq = lightcurve.LCQuery() #this object is used to query for the data
+        res = lcq.from_position(ra, dec, radius) # search for data which satisfies the beforementioned parameters
+        try:
+            # from the found dataset, access these select columns
+            ZTFdata = res.data[['mjd', 'mag', 'magerr', 'catflags', 'filtercode']] 
+        except:
+            # if this doesn't work, print error message
+            print(f"Something went wrong when extracting DataFrame: {iD}")
+            try:
+                # first search for the most common filtercode: this will determine which filter has the most observation points
+                # filter the data using the desired filter
+                f = dd.ZTFdata['filtercode'].mode()[0].compute()
+                ZTFdata = dd.ZTFdata.loc[ZTFdata['filtercode'] == f].compute()
+            except:
+                # if this doesn't work, print error message
+                print(f"Something went wrong when filtering for filter: {iD}")
+                try:
+                    # eliminate any points which are above a select number: M. Graham recommends to get rid of obvious spurious points
+                    ZTFdata = dd.ZTFdata.loc[ZTFdata['catflags']< 32768].compute()
+                except:
+                    # if this doesn't work, print error message
+                    print(f"Something went wrong when filerting catflags: {iD}")
+        finally:   
+            # when done with everything, always remove select columns
+            ZTFdata = ZTFdata.drop(['catflags','filtercode'],axis=1)
+    except:
+        # if the light curve could not be found, assign the data to None
+        print(f"Something went wrong with finding the light curve: {iD}")
+        ZTFdata = None
+    return ZTFdata # save the select light curve
+
+def lc_access(iD):
+    '''
+    Defines a function for acessing the light curve data based on equatorial coordinates of the LINEAR dataset.
+
+    Arguments:
+        iD (integer): ID of every light curve in the LINEAR dataset
+    '''
+    StarAttributes = data.targets[iD] # access attributes of a light curve from the LINEAR dataset
+    ra, dec = StarAttributes[3], StarAttributes[4] # access the equatorial coordinates
+    light = (iD,getZTFlightcurve(ra, dec, iD)) # search for light curve
+    return light # save the data
