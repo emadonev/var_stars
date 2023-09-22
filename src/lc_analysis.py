36e178f070416287899b1a89b72b6ec557429a47
Ema Donev <emadonev@icloud.com>
2023-09-22 18:11:05 +0200

    final changes to nbs and files

--- src/lc_analysis.py
+++ src/lc_analysis.py
@@ -289,7 +289,7 @@ def makeLCplot4(L1, L2, Z1, Z2, plotrootname='LCplot4', plotSave=False):
     # data
     xx, yy, zz = sort3arr(Z1['dataPhasedTime'], np.array(Z1['dataTemplate']), np.array(Z1['dataTemplateErr']))
     axs[1,0].errorbar(xx, yy, zz, fmt='.k', ecolor='gray', lw=1, ms=4, capsize=1.5, alpha=0.3)
-    # fit for Pztf
+    # fit for powertf
     axs[1,0].plot(Z1['modelPhaseGrid'], Z1['modTemplate'], 'black', markeredgecolor='black', lw=2, fillstyle='top', linestyle='solid')
     # fit for mean period
     axs[1,0].plot(Z2['modelPhaseGrid'], Z2['modTemplate'], 'red', markeredgecolor='red', lw=2, fillstyle='top', linestyle='solid')
@@ -313,88 +313,159 @@ def makeLCplot4(L1, L2, Z1, Z2, plotrootname='LCplot4', plotSave=False):
 
 # PERIODOGRAM ANALYSIS
 # ----------------------
-def periodogram_blazhko(power, limit, perc_xshift):
+def periodogram_blazhko(power, limit, perc_xshift, limit_percentage, verbose:bool=False):
     '''
     This function analyzes the peaks of periodograms from all RR Lyrae stars and determines if they are possible Blazhko stars.
 
     Arguments:
         power(array) arraw of power
         limit(float): a bar above which peaks are found
-        shift(integer): how big of a range you want to zoom into; # In % how much to cut the x-axis range
+        perc_xshift(float): in % how much to cut the x-axis range
+        limit_percentage(float): percentage of inclusion of subsequent peaks around the maximum
+        verbose(bool): testing mode, includes printing
     '''
-    matches = []
-    c = 0
-    peaks_inside = []
-    distance = []
-    cless = 0
-    cmore = 0
-    indicator = 2
-    x = 0
-    a = 0
     
-
-    while x != 1:
-        peaks, _ = find_peaks(power, height=limit)
-        if any(peaks) != True:
-            limit -= 0.05
-        else:
-            x = 1
-
-    if len(peaks)>50:
-        indicator = 0
+    # DEFINING LISTS AND COUNTERS
+    # -----------------------------
+    matches = [] # list for the x-axis and power value matches
+    peaks_inside = [] # list of all the peaks which are inside our cut range
+    distance = [] # a list of all the distances between peaks
+    peaks = [] # the starting list where the peaks will go
+    
+    a = 0 # counter for cutting the periodogram    
+    c = 0 # counter for counting peaks inside specified range
+    cless = 0 # counter for peaks before the max peak
+    cmore = 0 # counter for peaks after the max peak
+    indicator = np.nan # indicator of the blazhko effect
+    
+    # STEP 1: check if we have enough datapoints to even use
+    if len(power)<5: # if there are less than 5 datapoints
+        indicator = np.nan # assign the indicator as none
+        if verbose: print("Blazhko effect cannot be calculated.")
     else:
-
-        while a != 1:
-            max_peak_x = peaks[np.argmax(power[peaks])]
-            min_lim = max_peak_x - int((len(power) * perc_xshift))
-            max_lim = max_peak_x + int((len(power) * perc_xshift))
-            p = power[min_lim:max_lim]
+        # if enough datapoints, continue with peak finding!
+        
+        # STEP 2: finding peaks
+        while (len(peaks) == 0): # since we defined peaks as an empty list, it will stay that way until we find some peaks
+            peaks, _ = find_peaks(power, height=limit) # find peaks above a certain limit
+            limit -= 0.05 # lower the limit in case we find no peaks, if peaks found limit does not affect the code late
+        # if we are testing, print the number of peaks, the limiting factor and plot the periodogram
+        if verbose: 
+            print(f"The number of peaks:{len(peaks)}")
+            print(f"The limit:{limit}")
+            plt.plot(power)
+            plt.title("Full power spectrum")
+            plt.show()
             
-            if any(p) == False:
-                perc_xshift -= 0.05
-            elif len(p) == 1:
-                perc_xshift += 0.05
-            else:
-                a = 1
-
-        for x in range(len(peaks)):
-            matches.append((power[peaks][x], peaks[x]))
-
+        # CHECK 1: what if there are too many peaks --> indicator of very noisy and not good data
+        if len(peaks)>50: # if the number of peaks is too large
+            indicator = 0 # set the indicator to 0
+            if verbose: print(f"There are too many peaks >>> indicator is 0.")
+        else: # if not, continue with the function
         
-        for x in power[peaks]:
-            if x in p:
-                c += 1
-                peaks_inside.append(x)
-
-        x_peaks = []
-        for x in matches:
-            if x[0] in peaks_inside:
-                x_peaks.append(x[1])
-        x_peaks.remove(max_peak_x)    
-
-        
-        for i in x_peaks:
-            if max_peak_x<i:
-                distance.append(i-max_peak_x)
-                cmore += 1
-            else:
-                distance.append(max_peak_x-i)
-                cless += 1
-
-        if cmore>=1 and cless>=1:
-            if ((c-1)==2 or (c-1)==3):
-                indicator = 1
-            else:
-                within_range = [a for a in distance if a < 700]
-                if len(within_range) in [2, 3]:
-                    outof_range = [b for b in distance if b > 1000]
-                    if len(outof_range) == len(distance) - len(within_range):
-                        indicator = 1
-                    else:
-                        indicator = 0
+            # STEP 3: cutting the power spectrum to a specified range
+            while a != 1: # while our counter is not 1, meaning we set it to 1 when the range is defined and cut
+                
+                max_peak_x = peaks[np.argmax(power[peaks])] # finding the highest peak and its x coordinate
+                min_lim = max_peak_x - int((len(power) * perc_xshift)) # the lower limit is the x coordinate of the maximum value minus a certain percentage of the total data
+                max_lim = max_peak_x + int((len(power) * perc_xshift))# the higher limit is the x coordinate of the maximum value plus a certain percentage of the total data
+                p = power[min_lim:max_lim] # cutting the power spectrum
+                
+                # ADJUST 1: what if when lowering the percentage to cut we reach 0 because the maximum value is so close to the edges of the power spectrum?
+                if perc_xshift == 0.0: # if percentage is 0
+                    end_distance = len(power)-max_peak_x # calculating the end distance (from max_peak_x to the end of the list)
+                    if max_peak_x > end_distance: # if the max_peak is closer to the end of the list
+                        p = power[(max_peak_x-end_distance):] # cut from the distance to the end before the max peak all the way to the end
+                        if verbose: 
+                            print(f"The maximum value is too close to the end:{len(p)}")
+                            plt.plot(p)
+                            plt.title("The cut power spectrum")
+                            plt.show()
+                    else: # if the max peak is closer to the first element of the list
+                        p = power[0:(max_peak_x*2)] # cut from 0 to twice the max peak
+                        if verbose: 
+                            print(f"The maximum value is too close to the beginning:{len(p)}")
+                            plt.plot(p)
+                            plt.title("The cut power spectrum")
+                            plt.show()
+                    a = 1 # when finished with adjusting, break the loop
+                
+                # ADJUST 2: what if there were no values in peaks? --> the range is to large and then it cannot cut because the cut goes beyond the list
+                elif any(p) == False:
+                    perc_xshift -= 0.05 # lessen the percentage so the range falls inside the list
+                    perc_xshift = round(perc_xshift, 2) # make sure the percentage is rounded
+                    if verbose: print(f"Range is too large:{perc_xshift}")
+                # ADJUST 3: what if there is only 1 value for the peaks: the maximum peak --> the range is too small and cannot find other peaks
+                elif len(p) == 1:
+                    perc_xshift += 0.05 # enlarge the percentage so more peaks fall inside the range
+                    perc_xshift = round(perc_xshift, 2)# make sure the percentage is rounded
+                    if verbose: print(f"Range is too small:{perc_xshift}")
                 else:
-                    indicator = 0
-        else:
-            indicator = 0
+                    a = 1 # the process of adjusting is finished
+                    if verbose: 
+                        print(f"Range of cut:{len(p)}")
+                        plt.plot(p)
+                        plt.title("The cut power spectrum")
+                        plt.show()
+                    
+            # STEP 4: find the x values of the peaks inside the specified range
+            #--------------
+            # STEP 4a: create matches of the power of every peak and its x value
+            for x in range(len(peaks)):
+                matches.append((power[peaks][x], peaks[x]))
 
-    return indicator, limit
+            # STEP 4b: count the number of peaks inside the specified range (p)
+            for y in power[peaks]:
+                if y in p:
+                    c += 1
+                    peaks_inside.append(y)
+            if verbose: print(f"Number of peaks inside the cut range:{c}")
+            # STEP 4c: accessing the x values of the peaks inside the specified range
+            x_peaks = []
+            for j in matches: # for every match
+                if j[0] in peaks_inside: # if the y value is inside the range
+                    x_peaks.append(j[1]) # append the x value of the peak
+            x_peaks.remove(max_peak_x)    
+            
+            # STEP 5: counting the number of peaks before and after the maximum value: crucial for Blazhko Effect recognition
+            for i in x_peaks: # for every value inside the range
+                if max_peak_x<i: # after the max peak
+                    distance.append(i-max_peak_x) # calculate the distance from the peak to the max peak
+                    cmore += 1 # count number of peaks after the max peak
+                else:
+                    distance.append(max_peak_x-i) # calculate the distance from the peak to the max peak
+                    cless += 1 # count number of peaks before the max peak
+            if verbose: print(f"Number of peaks before:{cless}, and after:{cmore}")
+            # STEP 6: Determining if blazhko effect potentially present
+            #--------
+            # CHECK 2: there has to be at least one peak before and at least one after the main peak
+            if cmore>=1 and cless>=1:
+                # STEP 6a: if there are 2 or 3 peaks only in the range
+                if ((c-1)==2 or (c-1)==3):
+                    indicator = 1 # make the indicator as a positive mark
+                    if verbose: print(f"Blazhko effect indicated")
+                else:
+                    # STEP 6b: check if even though there are more peaks that the other peaks are far away from the 2 or 3 close to the max peak
+                    within_range = [a for a in distance if a < round((limit_percentage*len(p)))] # if the peaks within range have distances below 700 
+                    if verbose: print(f"Limiting number for peak counting:{round((limit_percentage*len(p)))}")
+                    if len(within_range) in [2, 3]: # if there are 2 or 3 peaks within the close range
+                        outof_range = [b for b in distance if b > round(((limit_percentage+0.2)*len(p)))] # calculate the amount of peaks outside of the range
+                        if verbose: 
+                            print(f"Limiting number for peak counting over the range:{round(((limit_percentage+0.2)*len(p)))}")
+                        if len(outof_range) == len(distance) - len(within_range): # assertion of number of the peaks
+                            indicator = 1 # set the indicator as positive
+                            if verbose: print(f"Blazhko effect indicated!")
+                        else:
+                            # if the assertion is incorrect, indicator is negative
+                            indicator = 0
+                            if verbose: print(f"Blazhko effect probably not present.")
+                    else:
+                        # if there are too many peaks withing range, set indicator to negative (0)
+                        indicator = 0
+                        if verbose: print(f"Blazhko effect probably not present.")
+            else:
+                # if there are not at least one peaks on each side, set indicator to 0
+                indicator = 0
+                if verbose: print(f"Blazhko effect probably not present.")
+
+    return indicator, limit, distance # save the indicator value,the limit and the distances
